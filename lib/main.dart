d9ce853c7b5887a6b71815faf6e3619ce08d40bd
sobdo <sob.dod@gmail.com>
2022-10-30 02:10:10 +0300

    Renamed app class

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,12 @@ Client client = Client()
 Account account = Account(client);
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RightPrice());
 
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RightPrice extends StatelessWidget {
+  const RightPrice({super.key});
 
   // This widget is the root of your application.
   @override
@@ -24,54 +24,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AuthPage(account)
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: AuthPage(account),
+      routes: {
+        '/auth': (context) => AuthPage(account),
+      },
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:osh_main_build/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const RightPrice());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
