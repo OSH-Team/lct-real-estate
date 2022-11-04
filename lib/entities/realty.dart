7252481ac998f4a93c9c46cc350b4b4ae527b016
hardw3q <sob.dod@gmai.com>
2022-11-04 20:13:14 +0300

    Added main entities

---
+++ lib/entities/realty.dart
@@ -0,0 +1,7 @@
+class Realty{
+  String address = '';
+  String coordinates = '';
+  String roominess = '';
+  int levelness = 1;
+  Realty(this.address, this.coordinates, this.roominess, this.levelness);
+}
---
+++ lib/parsing.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart';
+
+import 'entities/realty.dart';
+class Parsing{
+  String url = 'https://www.avito.ru/';
+  List<Realty> getMatchRealties(){
+    List<Realty> realties = [];
+
+    return realties;
+  }
+  List<String> _getMatchAddresses(){
+    List<String> matchAddresses = [];
+
+    return matchAddresses;
+  }
+}
