73ebf60b31e36f44d7f5f845b897a3a5fe24f8ac
SWYOD <67761337+SWYOD@users.noreply.github.com>
2022-11-05 00:58:12 +0300

    Main&Upload update.ver5

--- lib/pages/mainpage.dart
+++ lib/pages/mainpage.dart
@@ -1,5 +1,5 @@
 import 'dart:html';
-
+import 'package:sizer/sizer_ext.dart';
 import 'package:appwrite/appwrite.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
@@ -52,7 +52,8 @@ class MainPage extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(
+      body: SingleChildScrollView(
+       child: Column(
               children:[
                 /*Container(
                   decoration: BoxDecoration(
@@ -75,12 +76,12 @@ class MainPage extends StatelessWidget {
                       ],
                   ),
                 ),*/
-                  Container(
+                 Container(
                     decoration: BoxDecoration(
                       color: const Color(0xD9D9D9D9)
                     ),
-                    child: SizedBox(
-                      height: 700,
+                    child: Container(
+                      height: 1024,
                       child: PageView(
                         controller: controller,
                         scrollDirection: Axis.horizontal,
@@ -93,8 +94,10 @@ class MainPage extends StatelessWidget {
                       ),
                     ),
                   )
+
               ],
           )
+      )
     );
   }
 }
