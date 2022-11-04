9540ac1db5e3fadd420c2f2f6ec48d2ecec6412a
hardw3q <sob.dod@gmai.com>
2022-11-04 23:05:57 +0300

    Added InkWell

--- lib/pages/uploadpage.dart
+++ lib/pages/uploadpage.dart
@@ -1,23 +1,36 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:excel/excel.dart';
 class UploadPage extends StatelessWidget {
-  const UploadPage({Key? key}) : super(key: key);
-
+  UploadPage({Key? key}) : super(key: key);
+  FilePickerResult? pickedFile;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         children: [
           Container(
-            child: Container(
-              height: 451,
-              width: 722,
-              decoration: BoxDecoration(
-                border: Border.all(color: Color(0x66666666)),
-                borderRadius: BorderRadius.circular(30),
-                color: Color(0xACACACAC),
-              ),
-              child: Center(
-                  child: Text('uploadpage')
+            child: InkWell(
+              onTap: () async {
+                pickedFile = await FilePicker.platform.pickFiles(
+                  type: FileType.custom,
+                  allowedExtensions: ['xlsx'],
+                  allowMultiple: false,
+                );
+              },
+              child: Container(
+                height: 451,
+                width: 722,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color(0x66666666)),
+                  borderRadius: BorderRadius.circular(30),
+                  color: Color(0xACACACAC),
+                ),
+                child: Center(
+                    child: Text('uploadpage')
+                ),
               ),
             ),
           ),
